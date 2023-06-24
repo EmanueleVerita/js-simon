c6b3f116464fbc5b076933b210219a0f59156d88
EmanueleVerita <emanuele.verita@gmail.com>
2023-06-24 16:32:42 +0200

    faccio inseririe all'utente i numeri

--- js/script.js
+++ js/script.js
@@ -7,6 +7,21 @@ let printNumbers = document.querySelector('h1');
 printNumbers.innerHTML = numbers[0] + ' ' + numbers[1] + ' ' + numbers[2] + ' ' + numbers[3] + ' ' + numbers[4];
 
 
+setTimeout(hideNumbers , 10000);
+ 
+function hideNumbers (){
+    printNumbers.innerHTML = ' ';
 
+}
+
+// l'utente inserisce i numeri
+setTimeout (aksNumers , 15000);
+
+function aksNumers (){
+    for(let i = 0 ; i < numbers.length ; i++){
+        let userNumbers = parseInt(prompt('inserisci in numeri che hai appena visto in ordine'))
+        console.log('i tuoi numeri inseriti sono: ' , userNumbers);
+    }
+}
 
 
