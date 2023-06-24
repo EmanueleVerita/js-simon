60dad4b190201b03edb74fb4b92f92090a3d07ba
EmanueleVerita <emanuele.verita@gmail.com>
2023-06-24 18:30:01 +0200

    soluzione provvisoria

--- js/script.js
+++ js/script.js
@@ -15,13 +15,34 @@ function hideNumbers (){
 }
 
 // l'utente inserisce i numeri
+
 setTimeout (aksNumers , 15000);
 
 function aksNumers (){
+    const userNumbers = [];
+    let listNumbers;
     for(let i = 0 ; i < numbers.length ; i++){
-        let userNumbers = parseInt(prompt('inserisci in numeri che hai appena visto in ordine'))
-        console.log('i tuoi numeri inseriti sono: ' , userNumbers);
+        listNumbers = parseInt(prompt('inserisci in numeri che hai appena visto in ordine'));
+        console.log('lista numeri inseriti utente' , listNumbers);
+
+        userNumbers.push(listNumbers);
+        
     }
+
+    console.log('array numeri inseriti utente' , userNumbers);
+
+    //confronto i numeri scritti dall'utente con quelli dell'array iniziale
+
+    for(let j = 0 ; j < numbers.length ; j++){
+        if(numbers[j] == userNumbers[j]){
+            console.log('comlimenti hai indovinato tutti i numeri');
+        }
+        
+        console.log('purtroppo hai sbagliato :/');
+    }
+
+
+
 }
 
 
